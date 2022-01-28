224b54f90299a34fd3f59ba54ac259778cef4baa
Donghyun Lee <dominic2009@snu.ac.kr>
2022-01-29 00:49:24 +0900

    v2.0.1

---
+++ newarc.ex/WinRARHarness2.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+extern "C" __declspec(dllexport) __declspec(noinline) void fuzzme(char* lpFileName)
+{
+    printf("%s\n", lpFileName);
+}
+
+int main(int argc, char** argv)
+{
+    fuzzme(argv[1]);
+    return 0;
+}
