842651fa238af3d4b23800681f491cb906762298
Donghyun Lee <dominic2009@snu.ac.kr>
2022-01-29 00:48:09 +0900

    v2.0

---
+++ newarc.ex/Framework/strmix.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "FarPluginBase.hpp"
+#include "StringBase.hpp"
+
+void AddEndSlash(string& str);
+void CutToSlash(string& strStr, bool bInclude = false);
+void CutTo(string& str, TCHAR symbol, bool bInclude);
+void ConvertSlashes(string& strStr);
+
+void Quote(string& strStr);
---
+++ newarc.ex/ace.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//far
+#include "Framework/FarPluginBase.hpp"
+#include "Framework/StringBase.hpp"
+#include "Framework/strmix.hpp"
+
+//ace
+#include "ACE/ace.h"
+
+//newarc
+#include "module.hpp"
+
+class AceArchive;
+class AceModule;
+class AcePlugin;
+
+//self
+#include "ace.Plugin.h"
+#include "ace.Module.h"
+#include "ace.Archive.h"
